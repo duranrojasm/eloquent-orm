73989ff508c8365482356595e7b15fac3e237888
maria duran <duranrojasm@gmail.com>
2016-08-22 14:20:11 -0400

    Install Bootstrap

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,49 +10,8 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-use EloquentORM\User;
-use Faker\Factory as Faker;
 
-Route::get('/create', function () {
-    
-    $faker = Faker::create();
-    $user = User::create([
-        'name'  => $faker->name,
-        'email' => $faker->email,
-        'password' => bcrypt('123456'),
-        'gender' => $faker->randomElement(['f', 'm']),
-        'biography' => $faker->text(255)
-    ]);
-    return $user;
-});
-
-Route::get('/read/{id}', function($id){
-        
-    $user = User::find($id);
-    return $user;
-});
-
-Route::get('/update/{id}', function ($id) {
-    
-    $faker = Faker::create();
-    
-    //busca el usuario con el id 1. find(1);
-    $user = User::find($id);
-    
-    $user->name = $faker->name;
-    $user->gender = $faker->randomElement(['f', 'm']);
-    $user->biography = $faker->text(255);
-    
-    $user->save();
-    
-    return $user;
-});
-
-
-Route::get('/delete/{id}', function($id){
-        
-    $user = User::find($id);
-    $user->delete();
-    
-    return 'Usuario eliminado';
-});
+Route::get('/',[
+   'as'   => 'home',
+   'uses' => 'PagesController@home'
+]);
