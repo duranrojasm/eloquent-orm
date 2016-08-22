bbc21de6ffa72dc8dff02917f8c1bad2019cc51c
maria duran <duranrojasm@gmail.com>
2016-08-22 13:44:57 -0400

    User Seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use EloquentORM\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'name' => 'Linda Morales',
+            'gender' => 'f',
+        
+        ]);
+        
+        factory(User::class, 99)->create();
+    }
+}
